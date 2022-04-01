17daec79c90e52d7845c640ba46ad8a7e06f06b9
abritopach <adrbrpa1988@gmail.com>
2022-04-01 19:17:40 +0100

    🐛 Fixed problem with ionicons in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -7,5 +9,13 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  "framework": "@storybook/html"
+  "framework": "@storybook/html",
+  webpackFinal: config => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [{ from: './node_modules/ionicons/dist/ionicons/svg', to: './svg/' }],
+      })
+    );
+    return config;
+  }
 }
